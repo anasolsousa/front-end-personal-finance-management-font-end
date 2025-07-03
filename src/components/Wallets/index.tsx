a2c31198f4d2bbdfd7aad64f5e2a57fbf3ac7631
Ana Sousa <164207463+anasolsousa@users.noreply.github.com>
2025-07-03 12:43:29 +0100

    change layout

--- src/components/Wallets/index.tsx
+++ src/components/Wallets/index.tsx
@@ -4,93 +4,89 @@ import styles from "./styles.module.css";
 import { CirclePlus } from "lucide-react";
 import Grid from '@mui/material/Grid2';
 
+import { useTransaction } from "../../hooks/useTransaction";
+import { useAcconts } from "../../hooks/useAcconts";
+
+
 export function Wallets(){
 
-    type Card = {
-        name: string,
-        type: string,
-        amount: number,
-        user_id: string
-    }
-
-    const [cards, setCards] = useState<Card[]>([]);
-    const token = localStorage.getItem("token");
-    const userId = localStorage.getItem("userId");
-
-    useEffect(() => {
-        fetchCards();
-    }, []);
-
-    async function fetchCards() {
-        try{
-            const response = await fetch("http://localhost:8000/api/auth/account", {
-                method: "GET",
-                headers:{
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`,
-                }
-            });
-            if(response.ok){
-                const data = await response.json();
-                const filterByUser = data.filter((u:Card) => u.user_id == userId);
-                setCards(filterByUser);
-            }
-            else{
-                console.error("error")
-            }
-        } catch (error){
-            console.error(error)
-        }
-    }
+    const {transactions} = useTransaction();
+    const {account} = useAcconts();
 
     return(
-        <>
-        <h1>My Wallet</h1>
-            <div className={styles.grid}>
-                <div className={styles.section}>
-                    {/* spacing={1} => controla espacamento entre as colunas */}
-                    <Grid className={styles.containerGrid} container spacing={2}> 
-                        <div className={styles.card}>
-                            <p> Add Card +</p>
-                        </div>
-                        {cards.map((card, index) => (
-                            // Cada item ocupa 12/4 = 3 colunas em ecrãs grandes, 6 em médios e 12 em pequenos -> item xs={12} sm={6} md={4} lg={3}
-                            <div key={index}>
-                                <div className={styles.card}>
-                                    <div className={styles.gridColum1}>
-                                        <p className={styles.gridName}>{card.name}</p>
-                                        <p className={styles.gridType}>{card.type}</p>
-                                        <p className={styles.gridAmount}>{card.amount}€</p>
-                                    </div>
-                                    <div className={styles.gridColum2}>
-                                        <p><CirclePlus/></p>
-                                    </div>
-                                </div>
+        <main className={styles.container}>
+            <p className={styles.name_page}>Wallet</p>
+                <div className={styles.content}>
+                    <div className={styles.gridLeft}>
+                        <h1>My Wallet</h1>
+                            <div className={styles.cards}>
+                                <p className={styles.cardsTitle}> Add Card +</p>
+                                    {account.map((a, index) => (
+                                        <div key={index}>
+                                            <div className={styles.card}>
+                                                <div>
+                                                    <p className={styles.cardName}>{a.name}</p>
+                                                    <p className={styles.cardType}>{a.type}</p>
+                                                    <p className={styles.cardAmount}>{a.amount}€</p>
+                                                </div>
+                                                <div className={styles.gridColum2}>
+                                                    <p><CirclePlus/></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))}
                             </div>
-                        ))}
-                    </Grid>
-                </div>
-                <div className={styles.sectionContent}>
-                    <h1>Last Transaction</h1>
-                    <div className={styles.tableContainer}>
-                        <table>
-                            <thead>
-                                <tr className={styles.trHeader}>
-                                    <th>Category</th>
-                                    <th>Date</th>
-                                    <th>Amount</th>
-                                    <th>Type</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                  
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
-                </div>
-            </div>
-        </>
+                    <div className={styles.gridright}>
+                        <h1>Last Transaction</h1>
+                            <div className={styles.tableContainer}>
+                                <table className={styles.table}>
+                                    <thead className={styles.thead}>
+                                        <tr>
+                                            <th>Account</th>
+                                            <th className={styles.right}>Amount</th>
+                                            <th>Type</th>
+                                            <th>Category</th>
+                                            <th>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody className={styles.tbody}>
+                                        {transactions.map((transaction, index) => (
+                                            <tr
+                                                key={transaction.id}
+                                                className={`${styles.row} ${index % 2 === 0 ? styles.even : styles.odd}`}
+                                            >
+                                                <td>{account.find(a => a.id === transaction.account_id)?.name}</td>
+                                                <td
+                                                    className={`${styles.right} ${
+                                                        transaction.type === 'income' ? styles.income : styles.expense
+                                                    }`}
+                                                    >
+                                                    {transaction.type === 'income' ? '+' : '-'}
+                                                    {new Intl.NumberFormat('pt-PT', {
+                                                        minimumFractionDigits: 2,
+                                                        maximumFractionDigits: 2,
+                                                    }).format(transaction.amount)}€
+                                                </td>
+                                                <td>
+                                                    <span className={`${styles.typeBadge} ${
+                                                        transaction.type === 'income' ? styles.incomeBadge : styles.expenseBadge
+                                                    }`}>
+                                                        {transaction.type}
+                                                    </span>
+                                                </td>
+
+                                                <td className={styles.tooltipWrapper}>
+                                                    <span className={styles.tooltipTrigger}> {transaction.category.name}</span>
+                                                </td>
+                                                <td>{new Date(transaction.date).toLocaleDateString("pt-PT")}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+        </main>
     )
 }
